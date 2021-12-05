9c9e35c605d981ba3c4f3edf91365e49099127d2
Alex Gama <alexgama1612@gmail.com>
2021-12-05 03:32:47 +0000

    fixed grammar mistake in ping command, changed prefix to ?

--- Commands/Ping.js
+++ Commands/Ping.js
@@ -1,7 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 module.exports = {
     name: 'Ping',
     description: 'Checks bot latency!',
     execute(message, args) {
-        message.channel.send(`ping_pong: Latency is ${Date.now() - message.createdTimestamp} ms.API Latency is ${Math.round(client.ws.ping)} ms`);
+        message.channel.send(`:ping_pong: Latency is ${Date.now() - message.createdTimestamp} ms.API Latency is ${Math.round(client.ws.ping)} ms`);
     }
 }
