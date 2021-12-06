fac8bba7bc7704dbd636edac5c55e17af889f2fa
Alex Gama <alexgama1612@gmail.com>
2021-12-06 23:26:46 +0000

    added  commands

--- Commands/Ping.js
+++ Commands/Ping.js
@@ -1,9 +1,16 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-module.exports = {
-    name: 'Ping',
-    description: 'Checks bot latency!',
-    execute(message, args) {
-        message.channel.send(`:ping_pong: Latency is ${Date.now() - message.createdTimestamp} ms.API Latency is ${Math.round(client.ws.ping)} ms`);
-    }
-}
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const { Client, Collection, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+module.exports =
+{
+	async execute(interaction)
+	{
+		await interaction.reply(`:ping_pong: Latency is ${Date.now() - interaction.createdTimestamp} ms.`);
+	},
+
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Gives bot Ping')
+};
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ﻿const fs = require('fs');
 const { Client, Collection, Intents} = require("discord.js");
 const { token } = require('./config.json');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
