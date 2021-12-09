f80bb19f9fbb612509e0ee0187fef8170bf0b06e
Alex Gama <alexgama1612@gmail.com>
2021-12-09 23:43:10 +0000

    advanced citraroles more

--- Commands/citraroles.js
+++ Commands/citraroles.js
@@ -38,6 +38,24 @@ module.exports =
 			.setFooter(`${interaction.user.username} wishes to get a role`, avatar, true)
 			.setTimestamp()
 
+		const rolesEmbedGranted = new MessageEmbed()
+			.setColor('#8e5539')
+			.setTitle(`Choose your role`)
+			.setDescription('Here are the trading roles you can choose in CitraPSS')
+			.setThumbnail('https://cdn.discordapp.com/icons/592003559243907072/d96e0a691a3bba148f3ad91d286a36d4.webp?size=2048')
+			.addField(`Role Granted!`, '\u200b', false)
+			.setFooter(`${interaction.user.username} received a role`, avatar, true)
+			.setTimestamp()
+
+		const rolesEmbedRemoved = new MessageEmbed()
+			.setColor('#8e5539')
+			.setTitle(`Choose your role`)
+			.setDescription('Here are the trading roles you can choose in CitraPSS')
+			.setThumbnail('https://cdn.discordapp.com/icons/592003559243907072/d96e0a691a3bba148f3ad91d286a36d4.webp?size=2048')
+			.addField(`Role Removed!`, '\u200b', false)
+			.setFooter(`${interaction.user.username} removed a role`, avatar, true)
+			.setTimestamp()
+
 		//================================================================================================================================//
 
 		const initial = new MessageActionRow()
@@ -132,43 +150,126 @@ module.exports =
 
 		collector.on('collect', async i => {
 
-			if (i.customId === 'tRow') {
-				i.reply({ ephemeral: true, embeds: [tRolesEmbed], components: [trader] });
-			}
+			switch (i.customId)
+			{
+				case "tRow":
+					await i.update({ ephemeral: true, embeds: [tRolesEmbed], components: [trader] });
+					break;
 
-			if (i.customId === 'bRow') {
-				i.reply({ ephemeral: true, embeds: [bRolesEmbed], components: [battler] });
-			}
+				case "bRow":
+					await i.update({ ephemeral: true, embeds: [bRolesEmbed], components: [battler] });
+					break;
 
-			if (i.customId === 'tg6') {
-				guildMember.roles.add(['918517265320788008']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "tg4":
+					if (guildMember.roles.cache.some(role => role.id === '918624122211274762')){
+						guildMember.roles.remove(['918624122211274762']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [trader] });
+					}
+					else {
+						guildMember.roles.add(['918624122211274762']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [trader] });
+					}
+					break;
 
-			if (i.customId === 'tg7') {
-				guildMember.roles.add(['918517238540165180']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "tg5":
+					if (guildMember.roles.cache.some(role => role.id === '918624356383477820')) {
+						guildMember.roles.remove(['918624356383477820']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [trader] });
+					}
+					else {
+						guildMember.roles.add(['918624356383477820']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [trader] });
+                    }
+					break;
 
-			if (i.customId === 'tg8') {
-				guildMember.roles.add(['918517291065417810']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "tg6":
+					if (guildMember.roles.cache.some(role => role.id === '918517265320788008')) {
+						guildMember.roles.remove(['918517265320788008']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [trader] });
+					}
+					else {
+						guildMember.roles.add(['918517265320788008']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [trader] });
+                    }
+					break;
 
-			if (i.customId === 'bg6') {
-				guildMember.roles.add(['918517405599268945']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "tg7":
+					if (guildMember.roles.cache.some(role => role.id === '918517238540165180')) {
+						guildMember.roles.remove(['918517238540165180']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [trader] });
+					}
+					else {
+						guildMember.roles.add(['918517238540165180']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [trader] });
+                    }
+					break;
 
-			if (i.customId === 'bg7') {
-				guildMember.roles.add(['918517327937544223']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "tg8":
+					if (guildMember.roles.cache.some(role => role.id === '918517291065417810')) {
+						guildMember.roles.remove(['918517291065417810']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [trader] });
+					}
+					else {
+						guildMember.roles.add(['918517291065417810']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [trader] });
+                    }
+					break;
 
-			if (i.customId === 'bg8') {
-				guildMember.roles.add(['918517439657021531']);
-				i.reply({ content: 'Role Added!', ephemeral: true });
-			}
+				case "bg4":
+					if (guildMember.roles.cache.some(role => role.id === '918624449689968700')) {
+						guildMember.roles.remove(['918624449689968700']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [battler] });
+					}
+					else {
+						guildMember.roles.add(['918624449689968700']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [battler] });
+                    }
+					break;
+
+				case "bg5":
+					if (guildMember.roles.cache.some(role => role.id === '918624451459960842')) {
+						guildMember.roles.remove(['918624451459960842']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [battler] });
+					}
+					else {
+						guildMember.roles.add(['918624451459960842']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [battler] });
+                    }
+					break;
+
+				case "bg6":
+					if (guildMember.roles.cache.some(role => role.id === '918517405599268945')) {
+						guildMember.roles.remove(['918517405599268945']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [battler] });
+					}
+					else {
+						guildMember.roles.add(['918517405599268945']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [battler] });
+                    }
+					break;
+
+				case "bg7":
+					if (guildMember.roles.cache.some(role => role.id === '918517327937544223')) {
+						guildMember.roles.remove(['918517327937544223']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [battler] });
+					}
+					else {
+						guildMember.roles.add(['918517327937544223']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [battler] });
+                    }
+					break;
+
+				case "bg8":
+					if (guildMember.roles.cache.some(role => role.id === '918517439657021531')) {
+						guildMember.roles.remove(['918517439657021531']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedRemoved], components: [battler] });
+					}
+					else {
+						guildMember.roles.add(['918517439657021531']);
+						await i.update({ ephemeral: true, embeds: [rolesEmbedGranted], components: [battler] });
+                    }
+					break;
+            }
 
 		});
 
